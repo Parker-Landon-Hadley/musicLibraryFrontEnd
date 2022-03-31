9567cf69d26495dcd0f58f8c0272e11975d35989
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-31 17:09:31 -0400

    Creatd initial musictable.jsx

---
+++ src/Components/AddSong/AddSong.jsx
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import './AddSong.css'
+
+class AddSong extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+                title: '',
+                artist: '',
+                album: '',
+                genre: '',
+                release_date:'',    
+         }
+    }
+
+    handleChange = (event) =>{
+        console.log(event.target.value);
+        console.log(event.state)
+        this.setState({
+         [event.target.name] : event.target.value
+        })
+    }
+
+    handleSumbit = (event) =>{
+        event.preventDefault();
+        this.props.AddSong(this.state)
+    }
+
+    render() {
+        return ( 
+            <form className='newsong' onSubmit={this.handleSumbit}>
+                <h1>What's Your Favorite Song?</h1>
+                <label> Title </label>
+                <input  name="title" onChange={this.handleChange} value={this.state.title}/><br/>
+                <label> Artist </label>
+                <input name="artist" onChange={this.handleChange} value={this.state.artist} /><br/>
+                <label> Album </label>
+                <input name="album" onChange={this.handleChange} value={this.state.album} /><br/>
+                <label> Genre </label>
+                <input name="genre" onChange={this.handleChange} value={this.state.genre} /><br/>
+                <label> Release Date </label>
+                <input name="release_date" onChange={this.handleChange} value={this.state.release_date} /><br/>
+                <button className='button1' type='Submit'>Add New Song!</button>
+            </form>
+    )}
+}
+ 
+export default AddSong;
---
+++ src/Components/MusicTable/MusicTable.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+
+
+
+const MusicTable = (props) => {
+
+  
+
+  return (
+    
+
+   
+    
+      <table className="table table-bordered table-dark">
+        <thead >
+          <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Artist</th>
+          <th scope="col">Album</th>
+          <th scope="col">Genre</th>
+          <th scope="col">Release Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.musicList.map((e, i) => (
+            <tr key={i}>
+              <th scope="row">{e.title}</th>
+              <th scope="row">{e.artist}</th>
+              <th scope="row">{e.album}</th>
+              <th scope="row">{e.genre}</th>
+              <th scope="row">{e.releaseDate}</th>
+              <td>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    
+  );
+};
+
+export default MusicTable; 
+          
+
+            
+          

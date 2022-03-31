8ee501845f8a143e3e3de4bbf9ed0169a5cc250a
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-31 17:26:07 -0400

    Addressing Navbar issue

---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import './SearchBar.css'
+
+
+export default function SearchBar (props){
+    
+    return (
+      <div className="searchBar" >
+        <input placeholder="Search..." value={props.query} onChange={(e) => props.setQuery(e.target.value)} />
+      
+      </div>)
+   }

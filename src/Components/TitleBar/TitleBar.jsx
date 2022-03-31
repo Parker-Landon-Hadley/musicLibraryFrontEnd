e876f74911c43fe5be3d85ad24db512017aed22a
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-31 17:55:06 -0400

    Working on search bar

--- src/Components/TitleBar/TitleBar.jsx
+++ src/Components/TitleBar/TitleBar.jsx
@@ -1,15 +1,18 @@
-import './TitleBar.css';
+import React from 'react';
+import './TitleBar.css'
 
 const TitleBar = () => {
-    return ( 
-<div>
-    <h1 className="titleBar">
-        Music Libary
-    </h1>
-</div>
-
-     );
+    return (
+        <React.Fragment>
+            <div className='backgrd'>
+            <h2 className='navtitle'>MyMusic.</h2>
+            <div className='title'>
+                <h3>Welcome to <br/>
+                your music.</h3>
+                </div>
+            </div>
+        </React.Fragment>
+    );
 }
 
- 
 export default TitleBar;
